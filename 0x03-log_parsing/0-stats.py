8031efdcd1f1b6af6b35502932b12844ae397364
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-17 06:37:49 -0700

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,53 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics"""
 import sys
-import re
 
 
-def log_parsing():
+line_count = 0
+total_size = 0
+status_codes = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+
+
+def print_status():
     """
-    Reads stdin line by line and computes metrics.
-    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    Helper function to output status
     """
-    input_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '\
-                    r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
-    total_file_size = 0
-    line_count = 0
-    status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    print(f"File size: {total_size}")
 
-    try:
-        for line in sys.stdin:
-            match = re.match(input_pattern, line)
-            if match:
-                total_file_size += int(match.group(4))
-                file_code = int(match.group(3))
-                if file_code in status_code:
-                    status_code[file_code] += 1
-                line_count += 1
-                if line_count % 10 == 0:
-                    print_stat(total_file_size, status_code)
-        print_stat(total_file_size, status_code)
-    except KeyboardInterrupt:
-        print_stat(total_file_size, status_code)
-
-
-def print_stat(total_file_size, status_code):
-    print(f'File size: {total_file_size}')
-    for code, count in sorted(status_code.items()):
+    for code, count in status_codes.items():
         if count > 0:
-            print(f'{code}: {count}')
+            print(f"{code}: {count}")
 
 
-if __name__ == "__main__":
-    log_parsing()
+try:
+    for line in sys.stdin:
+        if line_count == 10:
+            print_status()
+            line_count = 0
+
+        line = line.rstrip()
+        line_parts = line.split()
+
+        if len(line_parts) > 4:
+            code = line_parts[-2]
+            total_size += int(line_parts[-1])
+
+            # Collate the number of lines per status code
+            if code in status_codes:
+                status_codes[code] += 1
+
+            # Increment the log count
+            line_count += 1
+except Exception:
+    pass
+finally:
+    print_status()
