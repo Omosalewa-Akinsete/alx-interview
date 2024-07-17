e62f2167a1e4f54ae518f3eead646918e7aeadb3
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-07-17 06:33:40 -0700

    Log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,50 +1,42 @@
 #!/usr/bin/python3
 """Reads stdin line by line and computes metrics"""
 import sys
-import signal
-
-total_size = 0
-status_counts = {
-    '200': 0,
-    '301': 0,
-    '400': 0,
-    '401': 0,
-    '403': 0,
-    '404': 0,
-    '405': 0,
-    '500': 0
-}
-line_count = 0
+import re
 
 
-def print_stats():
-    """Print status"""
-    print("File size: {}".format(total_size))
-    for code in sorted(status_counts.keys()):
-        if status_counts[code] > 0:
-            print("{}: {}".format(code, status_counts[code]))
+def log_parsing():
+    """
+    Reads stdin line by line and computes metrics.
+    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+    """
+    input_pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(.*?)\] '\
+                    r'"GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    total_file_size = 0
+    line_count = 0
+    status_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+    try:
+        for line in sys.stdin:
+            match = re.match(input_pattern, line)
+            if match:
+                total_file_size += int(match.group(4))
+                file_code = int(match.group(3))
+                if file_code in status_code:
+                    status_code[file_code] += 1
+                line_count += 1
+                if line_count % 10 == 0:
+                    print_stat(total_file_size, status_code)
+        print_stat(total_file_size, status_code)
+    except KeyboardInterrupt:
+        print_stat(total_file_size, status_code)
 
 
-def signal_handler(sig, frame):
-    """Signal handler"""
-    print_stats()
-    sys.exit(0)
+def print_stat(total_file_size, status_code):
+    print(f'File size: {total_file_size}')
+    for code, count in sorted(status_code.items()):
+        if count > 0:
+            print(f'{code}: {count}')
 
 
-signal.signal(signal.SIGINT, signal_handler)
-for line in sys.stdin:
-    line_count += 1
-    parts = line.split()
-    if len(parts) > 6:
-        file_size = parts[-1]
-        status_code = parts[-2]
-        if status_code in status_counts:
-            try:
-                total_size += int(file_size)
-                status_counts[status_code] += 1
-            except ValueError:
-                pass
-    if line_count % 10 == 0:
-        print_stats()
-
-print_stats()
+if __name__ == "__main__":
+    log_parsing()
