a2964042704930fd1de1856b083d553011f79d42
Omosalewa-Akinsete <shaleycoral@gmail.com>
2024-08-07 12:49:19 -0700

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,44 +1,53 @@
 #!/usr/bin/node
-
 const request = require('request');
-
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.error('Please provide a Movie ID as the first argument.');
+  console.log('Enter movie ID');
   process.exit(1);
 }
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('Failed to fetch movie data. Status code:', response.statusCode);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  const characters = data.characters;
-
-  characters.forEach((characterUrl) => {
-    request(characterUrl, (charError, charResponse, charBody) => {
-      if (charError) {
-        console.error('Error:', charError);
-        return;
+const fetchMovie = (url) => {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, res, body) => {
+      if (err) {
+        reject(err);
+      } else {
+        const movie = JSON.parse(body);
+        resolve(movie);
       }
-
-      if (charResponse.statusCode !== 200) {
-        console.error('Failed to fetch character data. Status code:', charResponse.statusCode);
-        return;
-      }
-
-      const characterData = JSON.parse(charBody);
-      console.log(characterData.name);
     });
   });
-});
+};
+
+const fetchCharacters = (movie) => {
+  return Promise.all(
+    movie.characters.map(async (characterUrl) => {
+      return new Promise((resolve, reject) => {
+        request.get(characterUrl, (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            const character = JSON.parse(body);
+            resolve(character.name);
+          }
+        });
+      });
+    })
+  );
+};
+
+const main = async () => {
+  try {
+    const movie = await fetchMovie(apiUrl);
+    const characters = await fetchCharacters(movie);
+
+    characters.forEach((actor) => console.log(actor));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+main();
